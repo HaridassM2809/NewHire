f805b18ee92af5825a37204b67fc228bcb6b8ed2
Haridass-M <haridassm2809@gmail.com>
2024-10-15 18:35:29 +0530

    Updated new hire flow

--- src/main/java/newhire_acu_codes/Newhire_ACU.java
+++ src/main/java/newhire_acu_codes/Newhire_ACU.java
@@ -2,4 +2,17 @@ package newhire_acu_codes;
 
 public class Newhire_ACU {
 
+	public static void main(String[] args) {
+		
+		System.out.println("Hi Java");
+		for (int i=0;i<6;i++)
+		{
+			for (int j=0;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+	}
 }
